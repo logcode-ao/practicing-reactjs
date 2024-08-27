4c81f3a5f83e36e04ae90f8bfa752efe5be5768e
César Filipe António <cesarfilipeantonio2404@gmail.com>
2024-08-27 10:54:53 +0100

    chose:adconando px no button no component InputSearch

--- app-selfwork/serch/src/components/InpotSearch.tsx
+++ app-selfwork/serch/src/components/InpotSearch.tsx
@@ -7,7 +7,7 @@ export function InputSearch() {
         type="text"
         className="h-8 flex-1 bg-transparent px-4 outline-none"
       />
-      <button className="h-12 w-10">
+      <button className="h-12 px-4">
         <Search className="size-5 mx-auto" />
       </button>
     </div>
