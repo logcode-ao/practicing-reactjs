3c8c7684693005af0200c73a356fabd3c9c19b46
César Filipe António <cesarfilipeantonio2404@gmail.com>
2024-09-07 15:20:37 +0100

    feat:criando components

--- app-teamwork/devstore/src/pages/search.tsx
+++ app-teamwork/devstore/src/pages/search.tsx
@@ -1,5 +1,51 @@
-import React from "react";
-
 export function Search() {
-  return <div>search</div>;
+  return (
+    <section className="px-8 flex flex-col gap-6 items-center justify-center">
+      <div className="flex gap-6 flex-wrap max-w-7xl">
+        <div className="size-[384px] border border-red-950 bg-[url('../public/products/moletom-ai-side.png')] bg-cover bg-center bg-zinc-900">
+          <div
+            className="fixed top-[296px] left-[104px]
+          border-zinc-500 border-2 w-60 flex gap-2 h-12 bg-black/60 rounded-full items-center justify-center"
+          >
+            <p className="text-sm text-zinc-100 w-[130px] truncate">
+              Moletom Nener Stop Lerning
+            </p>
+            <span className="font-semibold text-white bg-violet-500 rounded-full w-[83px] h-10 flex justify-center items-center">
+              R$123
+            </span>
+          </div>
+        </div>
+        <div className="size-[384px] border border-red-950 bg-[url('../public/products/moletom-ia-p-devs.png')] bg-cover bg-center bg-zinc-900">
+          <div className="border-zinc-500 border-2 w-60 flex gap-2 h-12 bg-black/60 rounded-full items-center justify-center">
+            <p className="text-sm text-zinc-100 w-[130px] truncate">
+              Moletom Nener Stop Lerning
+            </p>
+            <span className="font-semibold text-white bg-violet-500 rounded-full w-[83px] h-10 flex justify-center items-center">
+              R$123
+            </span>
+          </div>
+        </div>
+        <div className="size-[384px] border border-red-950 bg-[url('../public/products/moletom-java.png')] bg-cover bg-center bg-zinc-900">
+          <div className="border-zinc-500 border-2 w-60 flex gap-2 h-12 bg-black/60 rounded-full items-center justify-center">
+            <p className="text-sm text-zinc-100 w-[130px] truncate">
+              Moletom Nener Stop Lerning
+            </p>
+            <span className="font-semibold text-white bg-violet-500 rounded-full w-[83px] h-10 flex justify-center items-center">
+              R$123
+            </span>
+          </div>
+        </div>
+        <div className="size-[384px] border border-red-950 bg-[url('../public/products/moletom-never-stop-learning.png')] bg-cover bg-center bg-zinc-900">
+          <div className="border-zinc-500 border-2 w-60 flex gap-2 h-12 bg-black/60 rounded-full items-center justify-center">
+            <p className="text-sm text-zinc-100 w-[130px] truncate">
+              Moletom Nener Stop Lerning
+            </p>
+            <span className="font-semibold text-white bg-violet-500 rounded-full w-[83px] h-10 flex justify-center items-center">
+              R$123
+            </span>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
