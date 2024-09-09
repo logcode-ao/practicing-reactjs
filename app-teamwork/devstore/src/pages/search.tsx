d7200c0c1d365488a61d6ef055e89037e8a4da4a
César Filipe António <cesarfilipeantonio2404@gmail.com>
2024-09-09 18:36:32 +0100

    trocando o bg por tag imagem

--- app-teamwork/devstore/src/pages/search.tsx
+++ app-teamwork/devstore/src/pages/search.tsx
@@ -11,8 +11,9 @@ export function Search() {
           return (
             <div
               key={product.id}
-              className="flex justify-center items-end size-[384px] border border-red-950 bg-[url('../public/products/moletom-ai-side.png')] bg-cover bg-center bg-zinc-900"
+              className="flex justify-center items-end size-[384px] border border-red-950 bg-zinc-900"
             >
+              <img src={product.image} alt="Imagem dos altigos" />
               <CardDescricacao name={product.title} price={product.price} />
             </div>
           );
