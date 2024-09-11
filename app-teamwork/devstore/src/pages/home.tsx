29d00f2f9958d53aa99182a9ea2158f0643434f6
Edson João Gaspar <edsonjoaogaspar21@gmail.com>
2024-09-12 00:29:55 +0100

    feat: Primeiras alterações, adicionando imagens e construindo a home

--- app-teamwork/devstore/src/pages/home.tsx
+++ app-teamwork/devstore/src/pages/home.tsx
@@ -1,19 +1,32 @@
 import { Link } from "react-router-dom";
 import data from "../data/data.json";
+import moletom1 from "../assets/moletom1.png";
+import foto from "../assets/foto1.png";
 
 export function Home() {
   const { products } = data;
 
   return (
-    <div>
-      <h1 className="text-5xl">Home</h1>
-      {products.map((product) => (
-        <div key={product.id}>
-          <h1>{product.title}</h1>
-          <p>{product.price}</p>
-          <Link to={`/add-to-card/${product.id}`}>Detalhes</Link>
-        </div>
-      ))}
-    </div>
+    <main className="min-h-[100vh] text-white pt-8 pb-8 pr-10 pl-10 text bg-[#09090B]">
+      <div>
+        <header className="flex gap-3">
+          <h2 className="text-xl">devstore</h2>
+          <input type="text" placeholder="Buscar produtos" />
+          <ul className="flex gap-3">
+            <li>Cart(3)</li>
+            <li className="flex gap-1">
+              <span>Account</span> <img src={foto} />
+            </li>
+          </ul>
+        </header>
+        {products.map((product) => (
+          <div key={product.id}>
+            <h1>{product.title}</h1>
+            <p>{product.price}</p>
+            <Link to={`/add-to-card/${product.id}`}>Detalhes</Link>
+          </div>
+        ))}
+      </div>
+    </main>
   );
 }
