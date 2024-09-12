0e4f95f9c5e8aa60bfa8d6ca8fde59256a770db0
João Eduardo Panzo <joaoeduardopanzo@gmail.com>
2024-09-12 18:25:39 +0100

    chore: mudança na definiçao das routas

--- app-teamwork/devstore/src/App.tsx
+++ app-teamwork/devstore/src/App.tsx
@@ -1,27 +1,20 @@
 import "./App.css";
 
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Home } from "./pages/home";
 import { Search } from "./pages/search";
 import AddToCartPage from "./pages/add-to-cart-page";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-  },
-  {
-    path: "search",
-    element: <Search />,
-  },
-  {
-    path: "add-to-card/:id",
-    element: <AddToCartPage />,
-  },
-]);
-
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/add-to-card/:id" element={<AddToCartPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
