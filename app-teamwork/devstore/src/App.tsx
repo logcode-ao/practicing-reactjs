4051d2f5b370c9207535a0de2c4f2a0e9702df57
João Eduardo Panzo <joaoeduardopanzo@gmail.com>
2024-09-12 19:28:06 +0100

    feat: definiçao de layout e ajuste nas routas

--- app-teamwork/devstore/src/App.tsx
+++ app-teamwork/devstore/src/App.tsx
@@ -4,14 +4,17 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Home } from "./pages/home";
 import { Search } from "./pages/search";
 import AddToCartPage from "./pages/add-to-cart-page";
+import { LayoutRoot } from "./components/Layout";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/search" element={<Search />} />
-        <Route path="/add-to-card/:id" element={<AddToCartPage />} />
+        <Route path="/" element={<LayoutRoot />}>
+          <Route index element={<Home />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/add-to-card/:id" element={<AddToCartPage />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
---
+++ app-teamwork/devstore/src/components/Header.tsx
@@ -0,0 +1,44 @@
+import { Link, useNavigate } from "react-router-dom";
+import foto from "../assets/foto1.png";
+import { FormEvent } from "react";
+
+export function Header() {
+  const navigate = useNavigate();
+  const handleSearch = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    const data = Object.fromEntries(formData);
+
+    if (!data.search) {
+      return null;
+    }
+
+    navigate(`/search?q=${data.search}`);
+  };
+
+  return (
+    <header className="flex justify-between py-4 container mx-auto">
+      <div className="flex items-center gap-3">
+        <Link to={"/"}>
+          <h2 className="text-xl">devstore</h2>
+        </Link>
+        <form onSubmit={handleSearch}>
+          <input
+            type="text"
+            name="search"
+            placeholder="Buscar produtos"
+            className="bg-[#18181B] px-3 py-1 text-lg font-light rounded-full w-96"
+          />
+        </form>
+      </div>
+      <ul className="flex gap-3 items-center">
+        <li>Cart(3)</li>
+        <li>|</li>
+        <li className="flex gap-1 items-center">
+          <span>Account</span>{" "}
+          <img src={foto} className="size-6 cursor-pointer" />
+        </li>
+      </ul>
+    </header>
+  );
+}
